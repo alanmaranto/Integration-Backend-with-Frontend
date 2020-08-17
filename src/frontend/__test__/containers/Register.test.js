c636c2a1ef7f6fe7d0a14b35a2ecac09ff6797f6
alan <alanarostegui@hotmail.com>
2020-08-16 19:27:13 -0500

    tester send formulary

---
+++ src/frontend/__test__/containers/Register.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { mount } from "enzyme";
+import { create } from "react-test-renderer";
+import Register from "../../containers/Register";
+import ProviderMock from "../../__mocks__/providerMock";
+
+describe("<Register />", () => {
+  test("Register Form", () => {
+    const preventDefault = jest.fn();
+    const register = mount(
+      <ProviderMock>
+        <Register />
+      </ProviderMock>
+    );
+    register.find('form').simulate('submit', { preventDefault });
+    expect(preventDefault).toHaveBeenCalledTimes(1);
+    register.unmount();
+  });
+  test("Register snapshot", () => {
+    const register = create(
+      <ProviderMock>
+        <Register />
+      </ProviderMock>
+    );
+    expect(register.toJSON()).toMatchSnapshot()
+  });
+});
